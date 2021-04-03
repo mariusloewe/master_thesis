b8a321939ffad29c1c4bbd57285294a9f6d7a5fc
Unknown <marius.loewe@whu.edu>
2021-04-03 18:09:41 +0200

    change search params

--- execute.py
+++ execute.py
@@ -10,7 +10,10 @@ from src.helper.utils import _SMOTE
 # surpress FutureWarnings
 import warnings
 
-warnings.simplefilter(action="ignore", category=FutureWarning)
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+#warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
 def main():
@@ -20,9 +23,9 @@ def main():
     )
     logging.info("Start the run!")
     results_filepath = PurePath("results")  # TODO: Define the input/output paths
-    input_file_path = PurePath("input/input_file.xlsx")
-    target = TARGETS_BINARY[0]
-    task_type = "regression"
+    input_file_path = PurePath("input/20210403_input_file_new.xlsx")
+    target = TARGETS_BINARY[1]
+    task_type = "classification"
     first_search = PipelineSearch(
         file_path=input_file_path,
         target=target,
@@ -30,7 +33,7 @@ def main():
         results_filepath=results_filepath,
     )
 
-    first_search.search("DTC", sampler=_SMOTE) # , PCA=None, seed=SEED
+    first_search.search("XGB", sampler=_SMOTE) # , PCA=None, seed=SEED
 
 
 if __name__ == "__main__":
